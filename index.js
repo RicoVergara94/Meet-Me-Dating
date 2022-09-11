384b659c146ef77fdc539b626f0fa25930c91edb
Oscar Vergara <oscar.vergara1994@gmail.com>
2022-09-11 00:50:15 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let imageArray = [
+  "images/Ari.png",
+  "images/Des.png",
+  "images/Lin.png",
+  "images/Ethan.png",
+  "images/Cjmk.png",
+  // "images/Dream.png",
+  // "images/Catnips.png",
+  // "images/Kirby.png",
+  // "images/Charmed.png",
+  "images/Jules.png",
+  "images/Riza.png",
+  "images/Jerry.png",
+  // "images/Soze.png",
+  // "images/Chino.png",
+  "images/Tokio.jpeg",
+];
